a78fdbef645f2244e582032f3f2d4b8035da94cf
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-11-02 13:27:13 -0400

    zxc

--- main.ts
+++ main.ts
@@ -19,56 +19,18 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
-// loopCounter
-loopCounter = 8
-
 input.onButtonPressed(Button.A, function () {
-  while (loopCounter > 4)
-    // number 4 and 4 neopixels
-    loopCounter = loopCounter - 1
-    basic.showNumber(loopCounter)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.show()
-    basic.pause(500)
+  // loopCounter and setup
+  loopCounter = 4
+  basic.clearScreen()
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 
-    // number 3 and 3 neopixels
-    loopCounter = loopCounter - 1
-    basic.showNumber(loopCounter)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.show()
-    basic.pause(500)
-
-    // number 2 and 2 neopixels
-    loopCounter = loopCounter - 1
-    basic.showNumber(loopCounter)
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.show()
-    basic.pause(500)
-
-    // number 1 and 1 neopixels
-    loopCounter = loopCounter - 1
-    basic.showNumber(loopCounter)
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.show()
-    basic.pause(500)
-
-    // no neopixels and #0
-    loopCounter = loopCounter - 1
-    basic.showNumber(0)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    basic.pause(500)
-    }
-)
+  // 4 neo pixels
+  while (loopCounter >= 0)
+  basic.showNumber(loopCounter)
+  neopixelStrip.show()
+  pause(1000)
+})
